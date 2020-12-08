6bc0504176bf7a3274488ffa61944e6d62ee8958
Ben Jackson <puremourning@gmail.com>
2020-12-08 09:38:58 +0000

    Day 8; why did this take me so long...

---
+++ 2020/8/day8.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+
+Instruction = namedtuple( "Instruction", [ "inst", "operand" ] )
+
+filename = 'input.txt'
+
+
+class InfiniteLoop( Exception ):
+  pass
+
+
+def Run( program, patch_instruction = None ):
+  acc = 0
+  trace = []
+  pc = 0
+
+  while pc < len( program ):
+    if pc in trace:
+      raise InfiniteLoop(
+        f"Loop detected with accumulator: { acc } at pc { pc }" )
+
+    i = program[ pc ]
+    trace.append( pc )
+
+    if i.inst == 'acc':
+      acc += i.operand
+      pc += 1
+    elif i.inst == 'nop':
+      if pc == patch_instruction:
+        pc += i.operand
+      else:
+        pc += 1
+    elif i.inst == 'jmp':
+      if pc == patch_instruction:
+        pc += 1
+      else:
+        pc += i.operand
+
+  if pc >= len( program ):
+    print( f"End of program: accumulator: { acc }" )
+
+  # Finished
+  return acc
+
+
+program = []
+with open( filename ) as f:
+  lines = f.read().splitlines()
+
+for line in lines:
+  inst, operand = line.split( ' ', 1 )
+  operand = int( operand )
+  program.append( Instruction( inst, operand ) )
+
+print( "Part1:" )
+
+try:
+  acc = Run( program )
+except InfiniteLoop as e:
+  print( str( e ) )
+
+print( "Part2:" )
+
+for to_patch in range( len( program ) ):
+  try:
+    acc = Run( program, to_patch )
+    break
+  except InfiniteLoop:
+    pass
+
