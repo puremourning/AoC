9b490b713fa2937b7d67024bcbee77aa6b6bcd24
Ben Jackson <puremourning@gmail.com>
2020-12-10 13:32:44 +0000

    fix some comments

--- 2020/10/day10.py
+++ 2020/10/day10.py
@@ -22,21 +22,25 @@ print( f"1 jolt ( { diffs[ 1 ] } ) * 3 jolts ( { diffs[ 3 ] } ) "
 
 # Transitive closure of the graph of
 #
-# { 0 } -> { x }
+# { 0 } -> { jolts[ 0 ] }
 #   |        |
 #   |        v
-#   |----> { y }
+#   |----> { jolts[ 1 ] }
+#              ....
+#                |------>{ jolts[ -1 ] }
 #
 # We can calculate that with some dynamic programming
 
 costs = defaultdict( int )
 for i in range( len( jolts ) - 1, -1, -1 ):
-  # Cost for this cell is sum of all exit cells
   j = jolts[ i ]
+
   if i == len( jolts ) - 1:
+    # Cost for this cell is 1 (it's the last cell)
     costs[ j ] = 1
     continue
 
+  # Cost for this cell is sum of all posible exit cells
   cost = costs[ j + 1 ] + costs[ j + 2 ] + costs[ j + 3 ]
   costs[ j ] = cost
 
